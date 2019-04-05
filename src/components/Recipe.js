e76bbfab7d762042521d4faca56ddf3326f1a876
Mohamed-Ziyad <ziyad.web.developer@gmail.com>
2019-04-05 11:04:41 +0530

    15% completed

---
+++ src/components/Recipe.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Recipe extends Component {
+	render() {
+		return <React.Fragment>Salam</React.Fragment>;
+	}
+}
---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class RecipeDetails extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<h3>Recipe Details</h3>
+			</React.Fragment>
+		);
+	}
+}
